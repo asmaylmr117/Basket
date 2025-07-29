13a15fc43e00bfcea9db056ac900073f554201c7
asmaylmr117 <asmaylmr117@gmail.com>
2025-07-29 08:13:49 +0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import Home from './pages/Home/Home '; 
+import Home from './pages/Home/Home'; 
 import Shop from './pages/Shop/Shop';
 import Blog from './pages/Blog/Blog';
 import Contact from './pages/Contact/Contact';
