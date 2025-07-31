b31d956daeff3b98a44fa48b5bdf610bf79df8ce
mohamed589m <mohamed53634372@gmail.com>
2025-08-01 01:20:37 +0300

    header&footer

---
+++ src/utils/HeaderLinks.jsx
@@ -0,0 +1,32 @@
+export const profileLinks = [
+  { title: "My Plants", href: "/profile/selected-plants" },
+  { title: "My Posts", href: "/profile/my-posts" },
+];
+
+export const navLinks = [
+  {
+    title: "Home",
+    href: "/",
+  },
+  {
+    title: "Shop",
+    href: "/shop",
+  },
+  {
+    title: "Blog",
+    href: "/blog",
+  },
+  {
+    title: "Contact Us",
+    href: "/contact",
+  },
+
+  {
+    title: "About Us",
+    href: "/about",
+  },
+  {
+    title: "Checkout",
+    href: "/checkout",
+  },
+];
