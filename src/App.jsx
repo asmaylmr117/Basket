b79c4dc9e05958f0fb8d5ac86abb61df88e3850c
asmaylmr117 <asmaylmr117@gmail.com>
2025-07-31 12:14:37 +0300

    updat App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import Home from './pages/Home/Home'; 
-import Shop from './pages/Shop/Shop';
-import Blog from './pages/Blog/Blog';
-import Contact from './pages/Contact/Contact';
-import About from './pages/About/About';
-import Checkout from './pages/Checkout/Checkout';
+import Home from './Pages/Home/Home'; 
+import Shop from './Pages/Shop/Shop';
+import Blog from './Pages/Blog/Blog';
+import Contact from './Pages/Contact/Contact';
+import About from './Pages/About/About';
+import Checkout from './Pages/Checkout/Checkout';
 
 function App() {
   return (
