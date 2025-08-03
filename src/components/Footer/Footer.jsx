4f149f62f145522d267de22f4b2637ded0eabede
mohamed589m <mohamed53634372@gmail.com>
2025-08-03 22:59:57 +0300

    footer color

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,20 +4,20 @@ import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <div className="mt-20 bg-[#020618] text-[#fff]">
+    <div className="mt-20 bg-[#F7F8FD] text-[#fff]">
       <div className="mx-auto max-w-screen-xl px-4 pb-6 pt-16 sm:px-6 lg:px-8 lg:pt-16">
         <div className="newsletter-content flex items-center gap-4 md:justify-start justify-center border-b-[0.2px] border-[#fff] pb-16 mb-16">
           <div className="email-icon">
-            <IoCart className="text-6xl" />
+            <IoCart className="text-6xl text-[#71778E]" />
           </div>
-          <h4 className="newsletter-title text-dark-footerTitle font-bold md:text-3xl text-xl leading-[1.3]">
-            Sign Up to Get Updates & <br /> News About Us.
+          <h4 className="newsletter-title text-[#71778E] text-dark-footerTitle font-bold md:text-3xl text-xl leading-[1.3]">
+            Sign Up to Get Updates & <br className="text-[#71778E]" /> News About Us.
           </h4>
         </div>
 
         <div className="grid grid-cols-1 gap-20 lg:grid-cols-3">
           <div>
-            <p className="mt-1 max-w-md text-center leading-relaxed text-dark-footerSubTitle sm:max-w-xs sm:text-left">
+            <p className="mt-1 max-w-md text-center leading-relaxed text-[#71778E] sm:max-w-xs sm:text-left">
               You interested in fashion trends, the history of clothing,
               different types of garments, clothing materials, or something
               else? Let me know so I can tailor my response to your interests!
@@ -26,13 +26,13 @@ function Footer() {
 
           <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-4 lg:col-span-2">
             <div className="text-center sm:text-left">
-              <p className="text-2xl font-bold text-dark-footerTitle">
+              <p className="text-2xl text-[#71778E] font-bold text-dark-footerTitle">
                 About Us
               </p>
               <ul className="mt-8 space-y-4 text-sm">
                 <li>
                   <Link
-                    className="text-dark-footerSubTitle transition hover:text-[#35afa0]"
+                    className="text-[#71778E] transition hover:text-[#35afa0]"
                     to="/"
                   >
                     Home
@@ -40,7 +40,7 @@ function Footer() {
                 </li>
                 <li>
                   <Link
-                    className="text-dark-footerSubTitle transition hover:text-[#35afa0]"
+                    className="text-[#71778E] transition hover:text-[#35afa0]"
                     to="/aboutus"
                   >
                     About Us
@@ -48,7 +48,7 @@ function Footer() {
                 </li>
                 <li>
                   <Link
-                    className="text-dark-footerSubTitle transition hover:text-[#35afa0]"
+                    className="text-[#71778E] transition hover:text-[#35afa0]"
                     to="/blog"
                   >
                     Blog
@@ -58,13 +58,13 @@ function Footer() {
             </div>
 
             <div className="text-center sm:text-left">
-              <p className="text-2xl font-bold text-dark-footerTitle">
+              <p className="text-2xl text-[#71778E] font-bold text-dark-footerTitle">
                 Helpful Links
               </p>
               <ul className="mt-8 space-y-4 text-sm">
                 <li>
                   <Link
-                    className="text-dark-footerSubTitle transition hover:text-[#35afa0]"
+                    className="text-[#71778E] transition hover:text-[#35afa0]"
                     to="/shop"
                   >
                     Shop
@@ -72,7 +72,7 @@ function Footer() {
                 </li>
                 <li>
                   <Link
-                    className="text-dark-footerSubTitle transition hover:text-[#35afa0]"
+                    className="text-[#71778E] transition hover:text-[#35afa0]"
                     to="/contact"
                   >
                     Contact Us
@@ -80,7 +80,7 @@ function Footer() {
                 </li>
                 <li>
                   <Link
-                    className="text-dark-footerSubTitle transition hover:text-[#35afa0]"
+                    className="text-[#71778E] transition hover:text-[#35afa0]"
                     to="/checkout"
                   >
                     Checkout
@@ -90,14 +90,14 @@ function Footer() {
             </div>
 
             <div className="text-center sm:text-left">
-              <p className="text-2xl font-bold text-dark-footerTitle">
+              <p className="text-2xl text-[#71778E] font-bold text-dark-footerTitle">
                 Contact Us
               </p>
               <ul className="mt-8 space-y-4 text-sm">
                 <li>
                   <a
                     href="mailto:roofplanting1@gmail.com"
-                    className="flex items-center justify-center gap-1.5 ltr:sm:justify-start rtl:sm:justify-end hover:text-[#35afa0] text-dark-footerSubTitle"
+                    className="flex items-center justify-center gap-1.5 ltr:sm:justify-start rtl:sm:justify-end hover:text-[#35afa0] text-[#71778E]"
                   >
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -133,7 +133,7 @@ function Footer() {
                         d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
                       />
                     </svg>
-                    <span className="flex-1 text-dark-footerSubTitle">
+                    <span className="flex-1 text-[#71778E]">
                       +201234567890
                     </span>
                   </div>
@@ -159,7 +159,7 @@ function Footer() {
                       d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
                     />
                   </svg>
-                  <address className="-mt-0.5 flex-1 not-italic text-dark-footerSubTitle">
+                  <address className="-mt-0.5 flex-1 not-italic text-[#71778E]">
                     Tanta, Gharbia, Egypt
                   </address>
                 </li>
@@ -179,7 +179,7 @@ function Footer() {
       >
         <div className="text-center sm:text-left pt-4">
           <div className="text-sm text-slate-300">
-            <p className="flex text-md">
+            <p className="flex text-md text-[#71778E]">
               Copyright <FaRegCopyright className="mx-[5px] text-lg" /> 2025.
               All Rights Reserved.
             </p>
